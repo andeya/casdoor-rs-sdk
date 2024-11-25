4da75a15d0aa5fc313b1474e33eda5a13f50833b
DMoscicki <dimulgator@outlook.com>
2024-11-25 20:47:09 +0300

    feat: :closed_lock_with_key: add parse jwt tokens algos
    add functions which can parse different algorithms

--- src/authn/mod.rs
+++ src/authn/mod.rs
@@ -56,12 +56,43 @@ impl AuthSdk {
         .await?)
     }
 
-    pub fn parse_jwt_token(&self, token: &str) -> SdkResult<Claims> {
+    pub fn parse_jwt_token_rs256(&self, token: &str) -> SdkResult<Claims> {
         let mut validation = Validation::new(Algorithm::RS256);
         validation.set_audience(&[self.sdk.client_id()]);
 
         let td: TokenData<Claims> = jsonwebtoken::decode(token, &DecodingKey::from_rsa_pem(self.sdk.certificate().as_bytes())?, &validation)?;
         Ok(td.claims)
+    }
+
+    pub fn parse_jwt_token_rs512(&self, token: &str) -> SdkResult<Claims> {
+        let mut validation: Validation = Validation::new(Algorithm::RS512);
+        validation.set_audience(&[self.sdk.client_id()]);
+
+        let td: TokenData<Claims> = jsonwebtoken::decode(token, &DecodingKey::from_rsa_pem(self.sdk.certificate().as_bytes())?, &validation)?;
+
+        Ok(td.claims)
+    }
+
+    pub fn parse_jwt_token_es256(&self, token: &str) -> SdkResult<Claims> {
+        let mut validation: Validation = Validation::new(Algorithm::ES256);
+        validation.set_audience(&[self.sdk.client_id()]);
+
+        let decode_key = &DecodingKey::from_ec_pem(self.sdk.certificate().as_bytes())?;
+
+        let token_data: TokenData<Claims> = jsonwebtoken::decode(&token, &decode_key, &validation)?;
+
+        Ok(token_data.claims)
+    }
+
+    pub fn parse_jwt_token_es384(&self, token: &str) -> SdkResult<Claims> {
+        let mut validation: Validation = Validation::new(Algorithm::ES384);
+        validation.set_audience(&[self.sdk.client_id()]);
+
+        let decode_key = &DecodingKey::from_ec_pem(self.sdk.certificate().as_bytes())?;
+
+        let token_data: TokenData<Claims> = jsonwebtoken::decode(&token, &decode_key, &validation)?;
+
+        Ok(token_data.claims)
     }
 
     pub fn get_signin_url(&self, redirect_url: String) -> String {
@@ -124,5 +155,108 @@ impl AuthSdk {
             )
             .await?
             .into_data_default()
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use std::fs;
+
+    use crate::Config;
+
+    #[test]
+    fn succesfully_es256() {
+        let token = fs::read_to_string("./src/authn/testdata/tok_es256.txt").unwrap();
+        let cert = fs::read_to_string("./src/authn/testdata/cert_es256.txt").unwrap();
+        let cfg = Config::new(
+            "http://localhost:8000", 
+            "e953686f04e7055b698b", 
+            "secret", 
+            cert, 
+            "org_name", 
+            Some("app_name".to_owned()),
+        ).into_sdk();
+
+        let authnx = cfg.authn();
+
+        let tk = authnx.parse_jwt_token_es256(&token).unwrap();
+        assert_eq!("user1", tk.user.display_name);
+    }
+
+    #[test]
+    fn succesfully_es384() {
+        let token = fs::read_to_string("./src/authn/testdata/tok_es384.txt").unwrap();
+        let cert = fs::read_to_string("./src/authn/testdata/cert_es384.txt").unwrap();
+        let cfg = Config::new(
+            "http://localhost:8000", 
+            "e953686f04e7055b698b", 
+            "secret", 
+            cert, 
+            "org_name", 
+            Some("app_name".to_owned()),
+        ).into_sdk();
+
+        let authnx = cfg.authn();
+
+        let tk = authnx.parse_jwt_token_es384(&token).unwrap();
+        assert_eq!("user1", tk.user.display_name);
+    }
+
+    #[test]
+    fn succesfully_rs512() {
+        let token = fs::read_to_string("./src/authn/testdata/tok_rs512.txt").unwrap();
+        let cert = fs::read_to_string("./src/authn/testdata/cert_rs512.txt").unwrap();
+        let cfg = Config::new(
+            "http://localhost:8000", 
+            "e953686f04e7055b698b", 
+            "secret", 
+            cert, 
+            "org_name", 
+            Some("app_name".to_owned()),
+        ).into_sdk();
+
+        let authnx = cfg.authn();
+
+        let tk = authnx.parse_jwt_token_rs512(&token).unwrap();
+        assert_eq!("user1", tk.user.display_name);
+    }
+
+    #[test]
+    #[should_panic]
+    fn bad_algo_rs_tk256_cert512() {
+        let token = fs::read_to_string("./src/authn/testdata/tok_rs256.txt").unwrap();
+        let cert = fs::read_to_string("./src/authn/testdata/cert_rs512.txt").unwrap();
+        let cfg = Config::new(
+            "http://localhost:8000", 
+            "e953686f04e7055b698b", 
+            "secret", 
+            cert, 
+            "org_name", 
+            Some("app_name".to_owned()),
+        ).into_sdk();
+
+        let authnx = cfg.authn();
+
+        let _tk = authnx.parse_jwt_token_rs512(&token).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn bad_algo_es_tk256_cert512() {
+        let token = fs::read_to_string("./src/authn/testdata/tok_es256.txt").unwrap();
+        let cert = fs::read_to_string("./src/authn/testdata/cert_es384.txt").unwrap();
+        let cfg = Config::new(
+            "http://localhost:8000", 
+            "e953686f04e7055b698b", 
+            "secret", 
+            cert, 
+            "org_name", 
+            Some("app_name".to_owned()),
+        ).into_sdk();
+
+        let authnx = cfg.authn();
+
+        let _tk = authnx.parse_jwt_token_es384(&token).unwrap();
     }
 }
